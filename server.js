571313328a53924c631dc577064ba8e9f6cd139a
mjablonski984 <mrjab84@gmail.com>
2020-04-18 18:24:01 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
 
@@ -9,6 +10,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/meetings', require('./routes/meetings'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  // get all - place bellow all ather routes !
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
